1a2e151af35bcd1cc2342e9b0137d95f54b23fbb
Mark E. Fuller <mark.e.fuller@gmx.de>
2020-06-26 14:49:20 +0200

    added images and links
    Signed-off-by: Mark E. Fuller <mark.e.fuller@gmx.de>

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,9 @@ myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/nox-cycle.png') {
       myImage.setAttribute ('src','images/hono-path.png');
-    } else {
+    } else if (mySrc === 'images/hono-path.png') {
+      myImage.setAttribute ('src','images/PES_CH3_HONO_1_lumpHONO.png');
+    } else if (mySrc === 'images/PES_CH3_HONO_1_lumpHONO.png') {
       myImage.setAttribute ('src','images/nox-cycle.png');
     }
 }
